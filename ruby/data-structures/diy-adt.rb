deb4fd42a5306b1cf43818cd7e2256d4a7873a2f
Joshua Urrea <jurrea619@gmail.com>
2021-07-12 16:57:40 -0700

    Implement Queue class, minor Stack class edits

--- ruby/data-structures/diy-adt.rb
+++ ruby/data-structures/diy-adt.rb
@@ -1,23 +1,24 @@
 # DIY ADTs homework assignment
 
 class Stack
+
+    # use an underlying array to represent stack
     def initialize
-        # use an underlying array to represent stack
-        @arr = []
+      @arr = []
     end
 
+    # adds an element to the stack
     def push(el)
-      # adds an element to the stack
       arr << el
     end
 
+    # removes one element from the stack
     def pop
-      # removes one element from the stack
       arr.pop
     end
 
+    # view next element in stack
     def peek
-      # returns, but doesn't remove, the top element in the stack
       return arr.last
     end
 
@@ -25,12 +26,55 @@ class Stack
     attr_reader :arr
 end
 
-# Stack Testing
 # stack = Stack.new
 # stack.push(5)
 # stack.push(12)
 # stack.push(3)
 # p stack.pop # return 3
 # p stack.peek # return 12
+
+
+# Queue class
+class Queue
+
+    # initialize underlying array to represent queue
+    def initialize
+      @queue = []
+    end
+
+    # add element to queue
+    def enqueue(el)
+      queue << el
+    end
+
+    # remove element from front of queue
+    def dequeue
+      queue.shift
+    end
+
+    # view next element in queue
+    def peek
+      queue.first
+    end
+
+    # customize inspect return value
+    def inspect
+      queue.inspect
+    end
+
+    private
+    attr_reader :queue
+end
+
+# queue = Queue.new
+# queue.enqueue("hi")
+# queue.enqueue("there")
+# queue.enqueue("bye")
+# queue.enqueue("now")
+# p queue
+# p queue.dequeue
+# p queue
+# p queue.peek
+# p queue
 
 
