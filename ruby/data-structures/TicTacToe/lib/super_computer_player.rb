8cd5532e1caa2b91f988da9eed6dd769b46a3ce4
Joshua Urrea <jurrea619@gmail.com>
2021-07-25 23:00:04 -0700

    Implemented move method to finish SuperComputerPlayer class. Phase II and assignment complete

--- ruby/data-structures/TicTacToe/lib/super_computer_player.rb
+++ ruby/data-structures/TicTacToe/lib/super_computer_player.rb
@@ -1,13 +1,30 @@
 require_relative 'tic_tac_toe_node'
-
+=begin
+  SuperComputerPlayer is an extension of ComputerPlayer that uses the TicTacToeNode
+  class search in all possible moves from current state in order to win or avoid
+    a loss as much as possible
+=end
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    # create node with given game and mark 
+    node = TicTacToeNode.new(game.board, mark)
+    # if any child node of current state winning node, return prev_move_pos
+    node.children.each do |child_node|
+      return child_node.prev_move_pos if child_node.winning_node?(mark)
+    end
+    # no winning node, so find random node that does not lose us game
+    shuffled_moves = node.children
+    shuffled_moves.each do |node|
+      return node.prev_move_pos if !node.losing_node?(mark)
+    end
+    # raise error if no moves will avoid loss
+    raise "No non-losing nodes :/"
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Play the brilliant computer!"
-  hp = HumanPlayer.new("Jeff")
+  hp = HumanPlayer.new("Joshua")
   cp = SuperComputerPlayer.new
 
   TicTacToe.new(hp, cp).run
