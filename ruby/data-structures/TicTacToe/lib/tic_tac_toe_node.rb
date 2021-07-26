cb6e83ffe7afe71fa137f3508a1e90c9a3cd73b9
Joshua Urrea <jurrea619@gmail.com>
2021-07-25 22:20:29 -0700

    Implement losing_node? and winning_node? methods

--- ruby/data-structures/TicTacToe/lib/tic_tac_toe_node.rb
+++ ruby/data-structures/TicTacToe/lib/tic_tac_toe_node.rb
@@ -11,21 +11,37 @@ class TicTacToeNode
     @prev_move_pos = prev_move_pos
   end
 
+  # method to check if current state is lost, or could still lose
   def losing_node?(evaluator)
+    # if board is over AND winner is not us, we lost
+    if board.over?
+      return true if board.won? && board.winner != evaluator
+      return false
+    end
+
+    # if our turn, check if all children are losing nodes
+    if next_mover_mark == evaluator
+      children.all? { |node| node.losing_node?(evaluator) }
+    else  # other player's turn, ONE of nodes children is a possible losing node
+      children.any? { |node| node.losing_node?(evaluator) }
+    end
   end
+
 
   def winning_node?(evaluator)
-  end
+    # if game over and current player is winner, return true. False otherwise
+    if board.over?
+      return true if board.won? && board.winner == evaluator
+      return false
+    end
 
-=begin
-  Write a method children that returns nodes representing all the potential game
- states one move after the current node. To create this method, it will be 
- necessary to iterate through all positions that are empty? on the board object. 
- For each empty position, create a node by duping the board and putting a 
- next_mover_mark in the position. You'll want to alternate next_mover_mark so 
- that next time the other player gets to move. Also, set prev_move_pos to the 
- position you just marked, for reasons that will make sense when we use it later
-=end
+    # if our turn, true if any child is winning node.
+    if next_mover_mark == evaluator
+      children.any? { |node| node.winning_node?(evaluator)}
+    else # other players turn, true if all children are winning nodes
+      children.all? { |node| node.winning_node?(evaluator)}
+    end
+  end
 
   # This method generates an array of all moves that can be made after
   # the current move.
