7cfe8880480e19d1a6872008f5875c8d0b62ed61
Joshua Urrea <jurrea619@gmail.com>
2021-11-01 13:20:51 -0700

    Octupus Problems initial commit. Complete Quadratic search

---
+++ ruby/big-o/octupus-problems.rb
@@ -0,0 +1,20 @@
+=begin
+    Octupus problems set regarding Big-O analysis
+=end
+
+fish = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+
+# 'Sluggish Octupus' with quadratic n^2 runtime
+def sluggish(arr)
+    arr.each_with_index do |str, i|
+        longest = true
+        arr.each_with_index do |str2, j|
+            next if i == j
+            longest = false if str2.length > str.length
+        end
+        return [str, if] if longest
+    end
+end
+
+p sluggish(fish)
+
