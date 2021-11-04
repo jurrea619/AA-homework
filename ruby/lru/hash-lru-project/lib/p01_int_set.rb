02b18a99ad59a3188e2579ce04ec9b63bd93b41d
Joshua Urrea <jurrea619@gmail.com>
2021-11-04 15:58:05 -0700

    Implement ResizeIntSet. Phase 1 Complete

--- ruby/lru/hash-lru-project/lib/p01_int_set.rb
+++ ruby/lru/hash-lru-project/lib/p01_int_set.rb
@@ -73,7 +73,7 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_accessor :count, :store
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
@@ -81,18 +81,29 @@ class ResizingIntSet
   end
 
   def insert(num)
+    # false if item already exists in set
+    return false if include?(num)
+    self[num] << num
+    @count += 1
+    resize! if count > num_buckets
+    return true
   end
 
   def remove(num)
+    if self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -100,5 +111,13 @@ class ResizingIntSet
   end
 
   def resize!
+    # get length
+    length = @store.length
+    # create new array with 2 x length spaces
+    old_store = store
+    @count = 0
+    @store = Array.new(num_buckets * 2) { Array.new }
+    # reinsert all objects from original array
+    old.flatten.each { |num| insert(num) }
   end
 end
