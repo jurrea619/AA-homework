d32c6c870bffebc4af55e648f8a0b63c8ac0b6ce
Joshua Urrea <jurrea619@gmail.com>
2021-11-04 13:56:12 -0700

    Implement IntSet

--- ruby/lru/hash-lru-project/lib/p01_int_set.rb
+++ ruby/lru/hash-lru-project/lib/p01_int_set.rb
@@ -39,23 +39,32 @@ end
 
 
 class IntSet
+  attr_reader :store
+
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
+  # return true if number inserted, false otherwise
   def insert(num)
+    false if include?(num)
+    self[num] << num
+    true
   end
 
   def remove(num)
+    self[num].delete(num) if include?(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    self.store[num % num_buckets]
   end
 
   def num_buckets
